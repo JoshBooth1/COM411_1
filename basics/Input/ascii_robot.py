58a259070b8bcea30e922348d6554d0cee0417ed
Your Name <5bootj38@solent,ac,uk>
2021-10-08 12:09:04 +0100

    Added code to display an ASCII robot with dynamic eyes.

---
+++ basics/Input/ascii_robot.py
@@ -0,0 +1,7 @@
+# ask the user to select a character for the eyes
+print("Please enter a character for the eyes")
+eyes = input()
+print("##########")
+print(f"#  {eyes}  {eyes}  #")
+print("#  ----  #")
+print("##########")
---
+++ basics/Input/multiline_message.py
@@ -0,0 +1,11 @@
+# Display message to the standard output
+print("System Failure Imminent!")
+print("System reboot has been initiated...")
+print("")
+print("")
+print("...Rebooting sensory system")
+print("...Rebooting output motors")
+print("...Rebooting hover engine")
+print("")
+print("System online.")
+print("\t\"I am Beep\"")
