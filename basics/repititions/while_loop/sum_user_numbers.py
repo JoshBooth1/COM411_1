df8a00cb2b8f04b5917deea265eed321db87d442
Your Name <5bootj38@solent,ac,uk>
2021-11-05 11:49:25 +0000

    Added comments to the code

--- basics/repititions/while_loop/sum_user_numbers.py
+++ basics/repititions/while_loop/sum_user_numbers.py
@@ -1,11 +1,13 @@
 print("How many numbers should I add up?")
 numbers_to_sum = int(input())
+# Define variables
 summed_numbers = 1
 total = 0
+# Gather inputs from user based on the numbers_to_sum
 while summed_numbers <= numbers_to_sum:
     print(f"Please enter number {summed_numbers} of {numbers_to_sum}:")
     number = int(input(""))
     total = total + number
     summed_numbers = summed_numbers + 1
-
+# Display the output
 print(f"The total is {total}")
