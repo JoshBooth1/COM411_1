e58e9b926d2e6dcd08b2f3999c9c234b6a0392f7
Your Name <5bootj38@solent,ac,uk>
2021-11-05 12:06:18 +0000

    Added code comments

--- basics/repititions/while_loop/factorial.py
+++ basics/repititions/while_loop/factorial.py
@@ -1,9 +1,11 @@
+# Ask for users input for the number
 print("Please enter a number")
 number = int(input())
-
+# Define the variables
 count = 0
 total = 1
 
+# Calculate the factorial
 while count < number:
     count = count + 1
     total = total * count
