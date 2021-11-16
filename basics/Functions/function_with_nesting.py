96db14fec97f967cc1498eb01944f55ecf3a951a
Your Name <5bootj38@solent,ac,uk>
2021-11-16 13:24:47 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision

---
+++ basics/Functions/function_with_nesting.py
@@ -0,0 +1,12 @@
+# Define the function
+def action():
+    print("What lies before us?")
+    response = input()
+    if response == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine")
+
+
+# Call the function
+action()
